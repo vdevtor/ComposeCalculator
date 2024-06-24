2b27fc96f4ebbb7a3e254d81b7d7877156fa7e54
vitor <vitaoosp@gmail.com>
2024-06-24 01:40:48 -0300

    - adding releases notes - firebase integration

--- app/src/main/java/com/vitorthemyth/composecalculator/MainActivity.kt
+++ app/src/main/java/com/vitorthemyth/composecalculator/MainActivity.kt
@@ -9,7 +9,7 @@ import com.vitorthemyth.composecalculator.ui.theme.MaterialCalculatorTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Constants.BASE_URL
+
         setContent {
             MaterialCalculatorTheme {
                 CalculatorScreen()
